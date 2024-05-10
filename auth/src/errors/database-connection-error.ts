18853de7e371dbf3b9ccce8dea37c0df317e7c20
Andy Vo <andyvo190998@gmail.com>
2024-05-10 15:53:28 +0200

    feat: error handling with express

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,18 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
     reason = 'Error connection to database'
+    statusCode = 500
 
     constructor() {
-        super()
+        super('error connecting database')
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+    }
+
+    serializeErrors () {
+        return [
+            { message: this.reason }
+        ]
     }
 }
--- auth/src/middlewares/eror-handler.ts
+++ auth/src/middlewares/eror-handler.ts
@@ -1,16 +1,12 @@
 import { Response, Request, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof RequestValidationError) {
-        console.log('Handling this error as a request validation err')
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() })
     }
 
-    if (err instanceof DatabaseConnectionError) {
-        console.log('handling this error as a database err')
-    }
     res.status(400).send({
-        message: err.message
+        errors: [{ message: 'Something went wrong' }]
     })
 }
