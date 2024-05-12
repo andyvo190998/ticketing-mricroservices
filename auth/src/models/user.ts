8923f96fb5c2fabb8165fc08868164ac67844177
Andy Vo <andyvo190998@gmail.com>
2024-05-12 08:36:16 +0200

    feat: handle type for mongoose

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
+// interface that describes the properties that are required to create new User
+
+interface UserProps {
+    email: string;
+    password: string;
+}
+
+//interface that describes the props that user has
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserProps): any
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,6 +23,10 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema)
+userSchema.statics.build = (attrs: UserProps) => {
+    return new User(attrs)
+}
+
+const User = mongoose.model<any, UserModel>('User', userSchema)
 
 export { User }
