13a94e905337547414b3a734a9f19ea3a5ce07dc
Andy Vo <andyvo190998@gmail.com>
2024-06-19 13:37:45 +0200

    feat: making validateRequest middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,23 +1,23 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router()
 
-router.post('/api/users/signin',[
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid!'),
-    body('password')
-        .trim()
-        .notEmpty()
-        .withMessage('You must supply a password')
-], (req: Request, res: Response) => {
-    const errors = validationResult(req)
+router.post(
+    '/api/users/signin',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid!'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('You must supply a password')
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array())
-    }
 })
 
 
