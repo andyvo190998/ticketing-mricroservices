bbbb491a85ef1dd1d70d939b621e3e9267149bdd
Andy Vo <andyvo190998@gmail.com>
2024-06-19 14:21:20 +0200

    feat: handle signin auth service

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
+import jwt from 'jsonwebtoken';
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router()
 
@@ -16,8 +20,33 @@ router.post(
             .withMessage('You must supply a password')
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
+        const existingUser = await User.findOne({ email })
+
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials')
+        }
+
+        const passwordMatch = await Password.compare(existingUser.password, password)
+
+        if (!passwordMatch) {
+            throw new BadRequestError('Invalid credentials')
+        }
+
+        //Generate JWT
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!)
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        }
+
+        res.status(200).send(existingUser)
 })
 
 
