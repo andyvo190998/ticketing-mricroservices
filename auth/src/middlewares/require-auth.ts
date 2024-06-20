5ceddabf5fa097808350186a472f53fae42e5119
Andy Vo <andyvo190998@gmail.com>
2024-06-20 15:31:04 +0200

    feat: create requireAuth and currentUser middleware

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+import { NotAuthorizedError } from "../errors/no-auth-error";
+
+export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.currentUser) {
+        throw new NotAuthorizedError
+    }
+
+    next()
+}
