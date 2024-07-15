ffe0cc5c7b13d06c42076861847553d225e0f6aa
AndyVo <andy.vo@kiutra.com>
2024-07-15 15:43:16 +0200

    some new test for auth service

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,43 @@ it('return a 201 on successful signup', async () => {
         })
         .expect(201)
 })
+
+it('return a 400 with invalid email', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test123asdasd',
+        password: 'password'
+    })
+    .expect(400)
+})
+
+it('disallows duplicate email', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+})
+
+it('set a cookie after successful signup', async () => {
+    const response: any = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    expect(response.get('Set-Cookie')).toBeDefined()
+})
